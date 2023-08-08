c8adec3cc31a7f565a30399b538414295fd123a6
Jadon Sunshine <109299010+jadonsunshine@users.noreply.github.com>
2023-08-08 18:14:23 +0100

    first test

---
+++ SimpleStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract SimpleStorage {
+    //  Basic types in solidity: boolean ,uint, int, address, bytes
+
+bool hasFavouriteNumber = true;
+uint256 favouriteNumber = 88;
+string favouriteNumberInText = "Eigty-Eight";
+int256 favouriteInt = -88;
+address myAddress = 0xD0c2e5779d9D23964200330ed39af6781bD6b84D;
+bytes32 favouriteBytes = "mycat";
+
+function store (uint256 _favouriteNumber) public {
+    favouriteNumber = _favouriteNumber;
+}
+}

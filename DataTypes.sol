80f725d20446d72f2334d3f46d3db3c06c6225b9
Jadon Sunshine <109299010+jadonsunshine@users.noreply.github.com>
2023-08-10 15:53:17 +0100

    examples of data type

---
+++ DataTypes.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract DataTypes {
+    bool public favoriteBool = true;
+    int public favoriteInt = -8;
+    uint256 public favoriteUint = 256;
+    bytes public favoriteByte = "pig";
+    address public favoriteAddress;
+    string public favoriteString = "Sunshine";
+}

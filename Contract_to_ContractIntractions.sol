1156fd556ca1219bfbe2878182179f7e4df7da0d
Jadon Sunshine <109299010+jadonsunshine@users.noreply.github.com>
2023-09-22 23:56:46 +0100

    more learning

--- Contract_to_ContractIntractions.sol
+++ Contract_to_ContractIntractions.sol
@@ -1,4 +1,4 @@
-//SPDX=License-Identifier : Unlicensed
+//SPDX-License-Identifier:MIT
 pragma solidity ^0.8.17;
 
 import {SimpleStorage} from "./SimpleStorage.sol";
@@ -12,5 +12,22 @@ function createSimplestorageContract()public{
     SimpleStorage newsimpleStorageContract= new SimpleStorage();
     listOfSimpleStorageContracts.push(newsimpleStorageContract);
 }
-
-} 
+//now with each time you create by clicking the orange "create button" on the deploy page you create a new instance of the SimpleStorage contract
+ function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+    //two things needed for contract to contract interaction
+    //address
+    //ABI
+    SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+    mySimpleStorage.store(_newSimpleStorageNumber);
+    //this code below would still work exactly like the one above;
+      
+   listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+ }
+ function sfGet(uint256 _simpleStorageIndex)public view returns(uint256){
+    //this code would still work here
+     SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+     return mySimpleStorage.retrieve();
+     //here this way below
+    // return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+ }
+}

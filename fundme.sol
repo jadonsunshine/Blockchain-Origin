5550a0134b45b05969d8ebbb5f85c15b9f69e9d4
Jadon Sunshine <109299010+jadonsunshine@users.noreply.github.com>
2023-10-01 01:22:27 +0100

    data feeds

--- fundme.sol
+++ fundme.sol
@@ -1,18 +1,24 @@
-// SPDX-License-Identifier : MIT
+// SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-import {SimpleStorage} from "./SimpleFactoryinSimpleStorage";
-
-contract FundMe{
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interfaces.sol";
     uint256 public minimumUsd = 5;
-    function Fund(uint256 myvalue)  public payable {
-        myvalue = myfavouriteNumber * 10;
+    function Fund()  public payable {
+       
         require(msg.value >= minimumUsd , "Send an amount greater than 1Eth");
         //the require statement says that if the first statement fails revert to the second statement
     }
 
-    function withdraw(uint )public {
+//     function withdraw(uint )public payable {
 
-        require(msg.sender< 1e9);
+//         require(msg.value> 1e9,"Send enough Eth");
+//     }
+    function getPrice() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
     }
+    function getConversionRate() public {
+        //
+    }
+
 }
